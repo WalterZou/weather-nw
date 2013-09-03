c7990f4ea91cc4807adf4a4307b0c49a74e6ad32
WalterZou <olayi20u@gmail.com>
2013-09-03 18:48:02 +0800

    1

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Walter Zou
- * Date: 13-9-2
- * Time: 上午10:25
- * To change this template use File | Settings | File Templates.
- */
-var getDataByApi=require('./js/api.js');
-function getData(day,source){
-	var storage=window.localStorage;
-	if(storage){
-		var str=storage.getItem(source);
-		var data=JSON.parse(str).data[0];
-		for(var i= 0;i<data.length;i++){
-			if(data[i].date==day){
-					return data;
-			}
-		}
-	}else{
-		getDataByApi('2013-09-02');
-		getData(day,source);
-	}
+var controller=require('./js/controller'),
+	ejs=require('ejs'),
+	fs=require('fs');
+controller.getData('2013-09-03',renderTmpl);
+function renderTmpl(data){
+	console.log('render',data,typeof (data));
+	var tmpl=fs.readFileSync('./view/index.ejs','utf-8');
+	var output=ejs.render(tmpl,{mData:data});
+	insertHtml(output);
+}
+function insertHtml(html){
+	$('#wrapper').html(html);
 }
---
+++ js/controller.js
@@ -0,0 +1,70 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Walter Zou
+ * Date: 13-9-2
+ * Time: 上午10:25
+ * To change this template use File | Settings | File Templates.
+ */
+var http=require('http'),
+	fs=require('fs'),
+	path=require('path');
+var options={
+	key:'6cccedcf841019b3a6dee96205fa719c',
+	action:'moreday'
+};
+function getDataByApi(options){
+	var url='http://www.tianqiyubao.com/api/api.php?'+'key='+options.key+'&action='+options.action;
+	var req=http.get(url,function(res){
+		console.log("StatusCode:"+res.statusCode);
+		console.log("Headers:"+JSON.stringify(res.headers));
+		var str='';
+		res.on('data',function(chunk){
+			str+=chunk;
+		})
+		res.on('end',function(){
+			var data=JSON.parse(str);
+			data.update=new Date().Format("yyyy-MM-dd");
+			fs.writeFile(path.join(__dirname,'data.json'),JSON.stringify(data),function(err){
+				if(err) throw err;
+				console.log('数据写入成功');
+				getData();
+			})
+		})
+	}).on('error',function(e){
+			console.log("Error:"+ e.message);
+		});
+	req.end();
+}
+Date.prototype.Format = function (fmt) {
+	var o = {
+		"M+": this.getMonth() + 1, //月份
+		"d+": this.getDate(), //日
+		"h+": this.getHours(), //小时
+		"m+": this.getMinutes(), //分
+		"s+": this.getSeconds(), //秒
+		"q+": Math.floor((this.getMonth() + 3) / 3), //季度
+		"S": this.getMilliseconds() //毫秒
+	};
+	if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+	for (var k in o)
+		if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+	return fmt;
+}
+function getData(day,callback){
+	fs.readFile(path.join(__dirname,'data.json'),function(err,data){
+		if (err){
+			getDataByApi(options);
+			console.log('err',err);
+		}else{
+			data=JSON.parse(data.toString());
+			var nowDate=new Date().Format('yyyy-MM-dd');
+			if(data.update!=nowDate){
+				getDataByApi(options);
+			}else{
+				console.log('数据读取成功');
+				callback(data.data[0]);
+			}
+		}
+	})
+}
+exports.getData=getData;
