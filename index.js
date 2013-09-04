578705fe324793c46793f75f1eacdce8e063a1ad
WalterZou <olayi20u@gmail.com>
2013-09-04 14:50:52 +0800

    changes

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var controller=require('./js/controller'),
-	ejs=require('ejs'),
-	fs=require('fs');
-controller.getData('2013-09-03',renderTmpl);
-function renderTmpl(data){
-	console.log('render',data,typeof (data));
-	var tmpl=fs.readFileSync('./view/index.ejs','utf-8');
-	var output=ejs.render(tmpl,{mData:data});
-	insertHtml(output);
+var controller = require('./js/controller'),
+    ejs = require('ejs'),
+    fs = require('fs');
+
+function renderTmpl(data) {
+    var tmpl = fs.readFileSync('./view/index.ejs', 'utf-8'),
+        output = ejs.render(tmpl, {
+            mData: data
+        });
+    console.log(output);
+    insertHtml(output);
 }
-function insertHtml(html){
-	$('#wrapper').html(html);
+
+function insertHtml(html) {
+    $('#wrapper').html(html);
+    console.log(html);
 }
+controller.loadData(renderTmpl);
